e3bf5483d4975e22e94fe3480a5ed5bb3d75b73c
Thomas Bnt <thomasbnt@protonmail.com>
2020-11-28 14:23:12 +0100

    Updating some code

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 const Discord = require('discord.js')
 const config = require('./config.json')
-const colors = require("colors")
-const randomcolor = require('randomcolor')
-
 const truncate = function (str, length, ending) {
     if (length == null) {length = 100}
     if (ending == null) {ending = '...'}
@@ -16,40 +13,39 @@ const { prefix, WebhookRedditRSS } = config
 const WebhookRedditRSSc = new Discord.WebhookClient(WebhookRedditRSS.id, WebhookRedditRSS.token)
 
 const Watcher  = require('feed-watcher'),
-    feed = 'https://www.reddit.com/r/all.rss',
+    feed = 'https://dev.to/feed',
     interval = 10
 
 const watcher = new Watcher(feed, interval)
 
-const bot = new Discord.Client({
+const client = new Discord.Client({
     autoReconnect: true
 })
 
 // ---------------------- Ready ----------------------
-bot.on('ready', async ()  =>  {    
+client.on('ready', async ()  =>  {    
     watcher.start()
         .then(function(entries) {})
-        .catch(function(error) { console.log(logSymbols.error, error)})
+        .catch(function(error) { console.log(error)})
     watcher.on('new entries', function(entries) {
         entries.forEach(function(entry) {           
-            WebhookRedditRSSc.send(`[${entry.title}](${entry.link}) — Posté par ${entry.author}`)
+            WebhookRedditRSSc.send(`[${entry.title}](${entry.link}) — Published by ${entry.author}`)
             console.log('New entry \nTitle : ' + entry.title + 
             "\nURL : " + (entry.url || entry.link) + "\n\n")
         })
     })
-    console.log(" Connected to ".bgMagenta + bot.user.tag.bgMagenta + "\n")
+    console.log(`Connected to ${client.user.username} (ID : ${client.user.id})\n`)
+    client.user.setActivity('news', { type: 'WATCHING' }).catch(console.error)
+
 })
-function updatePresence() {
-    bot.user.setActivity("a feeeeeeed", {type: "WATCHING"})
-}
 
 // ---------------------- Messages ----------------------
-bot.on('message', (msg) => { 
+client.on('message', (msg) => { 
 
-    if (msg.author.bot) return
+    if (msg.author.client) return
     if(msg.channel.recipient) return
 
 })
 
-bot.login(config.token)
+client.login(config.token)
     .catch(e => console.error(e.message))
